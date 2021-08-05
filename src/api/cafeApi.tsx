12e2168404faf90d25b16fed70979752c01cfdbe
Jonathan Casillas <react.jonathan.casillas@gmail.com>
2021-08-05 14:29:02 -0500

    Se cambio la direccion IP de la api

--- src/api/cafeApi.tsx
+++ src/api/cafeApi.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const baseURL = 'http://192.168.0.26:8080/api'
+const baseURL = 'http://192.168.0.6:8080/api'
 
 const cafeApi = axios.create({
     baseURL
